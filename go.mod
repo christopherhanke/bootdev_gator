fc265e4e19011cd06e670c0a5ac9742d6b57066d
christopherhanke <christopherhanke@googlemail.com>
2024-10-23 21:46:08 +0200

    adding sql database via sqlc to the project.

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/christopherhanke/bootdev_gator
 
 go 1.23.2
+
+require github.com/google/uuid v1.6.0 // indirect
+
+require github.com/lib/pq v1.10.9 // indirect
---
+++ sql/queries/users.sql
@@ -0,0 +1,12 @@
+-- name: CreateUser :one
+insert into users (id, created_at, updated_at, name)
+values (
+    $1,
+    $2,
+    $3,
+    $4
+)
+returning *;
+
+-- name: GetUsers :one
+select * from users where name = $1 limit 1;
