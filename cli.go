669bb1edb2bcb23109229375d019f91cdc729835
christopherhanke <christopherhanke@googlemail.com>
2024-10-23 21:47:19 +0200

    implementing sql database in application. adding register handler. editing login to check for registered users.

--- cli.go
+++ cli.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	"time"
+	"unicode"
 
 	"github.com/christopherhanke/bootdev_gator/internal/config"
+	"github.com/christopherhanke/bootdev_gator/internal/database"
+	"github.com/google/uuid"
 )
 
 type state struct {
 	cfg *config.Config
+	db  *database.Queries
 }
 
 type command struct {
@@ -23,12 +30,49 @@ func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) > 1 {
 		return fmt.Errorf("commands arg slice is bigger than 1")
 	}
-	err := s.cfg.SetUser(cmd.args[0])
+	_, err := s.db.GetUsers(context.Background(), cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("user is not registered: %v", cmd.args[0])
+	}
+
+	err = s.cfg.SetUser(cmd.args[0])
 	if err != nil {
 		return err
 	}
 
 	fmt.Printf("User has been set: %v\n", s.cfg.CurrentUserName)
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	//Register user given in commands args slice
+	if len(cmd.args) < 1 {
+		return fmt.Errorf("commands arg slice is smaller than 1")
+	}
+	if len(cmd.args) > 1 {
+		return fmt.Errorf("commands arg slice is bigger than 1")
+	}
+	if len(cmd.args[0]) <= 1 {
+		return fmt.Errorf("no valid username given: %v (to short)", cmd.args[0])
+	}
+	if !isName(cmd.args[0]) {
+		return fmt.Errorf("no valid username given: %v (no char)", cmd.args[0])
+	}
+	user, err := s.db.CreateUser(
+		context.Background(),
+		database.CreateUserParams{
+			ID:        uuid.New(),
+			CreatedAt: sql.NullTime{Time: time.Now(), Valid: true},
+			UpdatedAt: sql.NullTime{Time: time.Now(), Valid: true},
+			Name:      cmd.args[0],
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("user already exists. couldn't create: %v", cmd.args[0])
+	}
+	s.cfg.SetUser(user.Name)
+	fmt.Println(user.ID, user.CreatedAt.Time, user.UpdatedAt.Time, user.Name)
+
 	return nil
 }
 
@@ -56,4 +100,13 @@ func (c *commands) run(s *state, cmd command) error {
 
 	f := c.CommandMap[cmd.name]
 	return f(s, cmd)
+}
+
+func isName(s string) bool {
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			return true
+		}
+	}
+	return false
 }
--- main.go
+++ main.go
@@ -1,20 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/christopherhanke/bootdev_gator/internal/config"
+	"github.com/christopherhanke/bootdev_gator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	cfg, err := config.Read()
-	if err != nil {
-		log.Fatalf("error reading config: %v\n", err)
-	}
-	fmt.Printf("Read config: %+v\n", cfg)
-
 	var currState state
 	bufferCfg, err := config.Read()
 	if err != nil {
@@ -23,10 +20,19 @@ func main() {
 		currState.cfg = &bufferCfg
 		fmt.Printf("Read state: %+v\n", *currState.cfg)
 	}
+	db, err := sql.Open("postgres", currState.cfg.DBURL)
+	if err != nil {
+		log.Fatalf("error open database: %v\n", err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+	currState.db = dbQueries
+
 	commands := commands{
 		make(map[string]func(*state, command) error),
 	}
 	commands.register("login", handlerLogin)
+	commands.register("register", handlerRegister)
 
 	args := os.Args
 	if len(args) < 2 {
@@ -45,7 +51,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	cfg, err = config.Read()
+	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v\n", err)
 	}
